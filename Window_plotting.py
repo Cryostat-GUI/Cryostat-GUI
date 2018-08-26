314cf7fcb677229cbe6942e2a8f9d7bed67a3260
bklebel <32774645+bklebel@users.noreply.github.com>
2018-08-26 20:19:23 +0200

    small changes

--- Window_plotting.py
+++ Window_plotting.py
@@ -1,8 +1,8 @@
 import sys
 from PyQt5 import QtGui, QtWidgets
 
-from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as NavigationToolbar
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 from matplotlib.figure import Figure
 
 import random
