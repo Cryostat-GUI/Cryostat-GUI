6941ae4bd85796bba7fb8ecedd87c7bedb62d518
Cryostat-GUI <43876961+Cryostat-GUI@users.noreply.github.com>
2018-11-10 20:44:07 +0100

    small progress on Sequence builder

--- qlistmodel.py
+++ qlistmodel.py
@@ -3,90 +3,91 @@ import sys
 from copy import deepcopy
 from pickle import dumps, load, loads
 from PyQt5.QtCore import QTimer
+from PyQt5 import QtCore
 # import math
 
-class PyMimeData(QtCore.QMimeData):
-    """ The PyMimeData wraps a Python instance as MIME data.
-    """
-    # The MIME type for instances.
-    MIME_TYPE = 'application/x-ets-qt4-instance'
+# class PyMimeData(QtCore.QMimeData):
+#     """ The PyMimeData wraps a Python instance as MIME data.
+#     """
+#     # The MIME type for instances.
+#     MIME_TYPE = 'application/x-ets-qt4-instance'
 
-    def __init__(self, data=None, **kwargs):
-        """ Initialise the instance.
-        """
-        super(PyMimeData, self).__init__(**kwargs)
+#     def __init__(self, data=None, **kwargs):
+#         """ Initialise the instance.
+#         """
+#         super(PyMimeData, self).__init__(**kwargs)
 
-        # Keep a local reference to be returned if possible.
-        self._local_instance = data
+#         # Keep a local reference to be returned if possible.
+#         self._local_instance = data
 
-        if data is not None:
-            # We may not be able to pickle the data.
-            try:
-                pdata = dumps(data)
-            except:
-                return
+#         if data is not None:
+#             # We may not be able to pickle the data.
+#             try:
+#                 pdata = dumps(data)
+#             except:
+#                 return
 
-            # This format (as opposed to using a single sequence) allows the
-            # type to be extracted without unpickling the data itself.
-            self.setData(self.MIME_TYPE, dumps(data.__class__) + pdata)
+#             # This format (as opposed to using a single sequence) allows the
+#             # type to be extracted without unpickling the data itself.
+#             self.setData(self.MIME_TYPE, dumps(data.__class__) + pdata)
 
-    @classmethod
-    def coerce(cls, md):
-        """ Coerce a QMimeData instance to a PyMimeData instance if possible.
-        """
-        # See if the data is already of the right type. If it is then we know
-        # we are in the same process.
-        if isinstance(md, cls):
-            return md
+#     @classmethod
+#     def coerce(cls, md):
+#         """ Coerce a QMimeData instance to a PyMimeData instance if possible.
+#         """
+#         # See if the data is already of the right type. If it is then we know
+#         # we are in the same process.
+#         if isinstance(md, cls):
+#             return md
 
-        # See if the data type is supported.
-        if not md.hasFormat(cls.MIME_TYPE):
-            return None
+#         # See if the data type is supported.
+#         if not md.hasFormat(cls.MIME_TYPE):
+#             return None
 
-        nmd = cls()
-        nmd.setData(cls.MIME_TYPE, md.data())
+#         nmd = cls()
+#         nmd.setData(cls.MIME_TYPE, md.data())
 
-        return nmd
+#         return nmd
 
-    def instance(self):
-        """ Return the instance.
-        """
-        if self._local_instance is not None:
-            return self._local_instance
+#     def instance(self):
+#         """ Return the instance.
+#         """
+#         if self._local_instance is not None:
+#             return self._local_instance
 
-        io = StringIO(str(self.data(self.MIME_TYPE)))
+#         io = StringIO(str(self.data(self.MIME_TYPE)))
 
-        try:
-            # Skip the type.
-            load(io)
+#         try:
+#             # Skip the type.
+#             load(io)
 
-            # Recreate the instance.
-            return load(io)
-        except:
-           pass
+#             # Recreate the instance.
+#             return load(io)
+#         except:
+#            pass
 
-        return None
+#         return None
 
-    def instanceType(self):
-        """ Return the type of the instance.
-        """
-        if self._local_instance is not None:
-            return self._local_instance.__class__
+#     def instanceType(self):
+#         """ Return the type of the instance.
+#         """
+#         if self._local_instance is not None:
+#             return self._local_instance.__class__
 
-        try:
-            return loads(str(self.data(self.MIME_TYPE)))
-        except:
-            pass
+#         try:
+#             return loads(str(self.data(self.MIME_TYPE)))
+#         except:
+#             pass
 
-        return None
+#         return None
 
 
 
-# class Node(object):
-#     """docstring for Node"""
-#     def __init__(self, data):
-#         super(Node, self).__init__()
-#         self.data = data
+# # class Node(object):
+# #     """docstring for Node"""
+# #     def __init__(self, data):
+# #         super(Node, self).__init__()
+# #         self.data = data
 
 
 
