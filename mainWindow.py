85f152a0d4fefb733a5cb4491a0be2a50b3c65f9
atezer <armin.tezer@gmail.com>
2018-11-13 17:21:47 +0100

    keithley 2.0.10

--- mainWindow.py
+++ mainWindow.py
@@ -1095,10 +1095,9 @@ class mainWindow(QtWidgets.QMainWindow):
                 
 
                 # calculating resistance whereas sourcname is either 'Keithley6221_1' or 'Keithley6221_2'
-              #  if 'GUI_Display' in kwargs:
-               #     sourcename = '{0:s}'.format(value.strip(')').split('(')[1])
-                #    GUI_Box.activated['str'].connect(lambda value: self.threads[threadname][0].calculate_resistance(self.data[sourcename]['Current_A'], self.data[dataname]['Voltage_V']))
-                 #   worker.sig_Infodata.connect(lambda data: self.store_data_Keithley(data, dataname, GUI_Display=kwargs['GUI_Display']))
+                if 'GUI_Display' in kwargs:
+                    GUI_Box.activated['str'].connect(lambda value: self.threads[threadname][0].calculate_resistance(self.data['{0:s}'.format(value.strip(')').split('(')[1])]['Current_A'], self.data[dataname]['Voltage_V']))
+                    worker.sig_Infodata.connect(lambda data: self.store_data_Keithley(data, dataname, GUI_Display=kwargs['GUI_Display']))
 
 
                 # setting Keithley values by GUI Keithley window
