5c446df2e1b3faa30b605f46270b93a9657ca7b5
Benjamin Klebel <32774645+bklebel@users.noreply.github.com>
2020-07-21 10:28:15 +0200

    livelogging improve performance
    test from all() is notably faster on generators than on comprehensions

--- CryostatGUI/loggingFunctionality/logger.py
+++ CryostatGUI/loggingFunctionality/logger.py
@@ -553,7 +553,7 @@ class live_Logger_bare(object):
                 for instr in self.data_live:
                     for varkey in self.data_live[instr]:
                         for calc in self.calculations:
-                            if all([x not in varkey for x in self.noCalc]):
+                            if all((x not in varkey for x in self.noCalc)):
                                 if self.time_init:
                                     self.calculations_perform(
                                         instr, varkey, calc, times)
