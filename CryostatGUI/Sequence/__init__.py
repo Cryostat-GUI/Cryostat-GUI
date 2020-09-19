4531227b9668902017fe01f5d17e7f4e7efd77f7
Cryostat-GUI <43876961+Cryostat-GUI@users.noreply.github.com>
2020-09-19 16:16:03 +0200

    debugging

--- CryostatGUI/Sequence/__init__.py
+++ CryostatGUI/Sequence/__init__.py
@@ -1,1 +1,7 @@
-from Sequence import *
+# from Sequence import *
+
+
+from .Sequence import Sequence_Thread
+from .Sequence import OneShot_Thread
+from .Sequence import OneShot_Thread_multichannel
+
