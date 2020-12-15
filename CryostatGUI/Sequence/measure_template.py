b6690171b8510c667045ae6dd0c3e422cb0b1a03
Cryostat-GUI <43876961+Cryostat-GUI@users.noreply.github.com>
2020-12-15 15:45:34 +0100

    zmq Sequence running measurement template

--- CryostatGUI/Sequence/measure_template.py
+++ CryostatGUI/Sequence/measure_template.py
@@ -1,20 +1,34 @@
 """Documentation for usable functions in measuring scripts, to be injected by a user"""
+# import pandas as pd
 
-# t1 = self.temperature_sample
+print("MEASURING -- MEASURING -- MEASURING")
+
+t1 = self.getTemperature_force(sensortype="sample")
 # rho, I, V = AbstractMeasureResistance()
-# t2 = self.temperature_sample
-# t_mean = np.mean(t1, t2)
-# t_std = np.std(t1, t2)
+
+res = self.query_device_command(
+    "SR860_1", command={"measure_Resistance": None}, retries_n1=10, retries_n2=5
+)
+
+t2 = self.getTemperature_force(sensortype="sample")
+
+t_mean = np.mean([t1, t2])
+t_std = np.std([t1, t2])
 # df = pd.DataFrame(
-#     dict(
-#         temperature=[t_mean],
-#         temperature_std=[t_std],
-#         rho=[rho["coeff"]],
-#         rho_residuals=[rho["residuals"]],
-#     )
+d = dict(
+    temperature=[t_mean],
+    temperature_std=[t_std],
+    rho=[res["SampleResistance_Ohm"]]
+    # rho=[rho["coeff"]],
+    # rho_residuals=[rho["residuals"]],
+)
 # )
 
-# with open(self.datafile, "a", newline="") as f:
-#     df.tail(1).to_csv(f, header=f.tell() == 0)
+self.datafile = "C:/Users/Lab-user/Dropbox/SPLITCOIL_data/tests/first.csv"
+self.measuring_store_data(data=d, datafile=self.datafile)
 
-print("Temperature:", self.getTemperature_force(sensortype="sample"))
+
+# with open(self.datafile, "a", newline="") as f:
+#     df.tail(1).to_csv(f, header=f.tell() == 0, index=False)
+
+# print("Temperature:", self.getTemperature_force(sensortype="sample"))
